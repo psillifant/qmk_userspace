fab821055171aa3637273040700266dc39b32db6
Peter Sillifant <peter@Peters-Laptop.local>
2024-05-05 17:29:21 +0100

    Added new layers for MacOS - no changes made yet.

--- keyboards/crkbd/rev1/keymaps/corne/keymap.c
+++ keyboards/crkbd/rev1/keymaps/corne/keymap.c
@@ -18,8 +18,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
-#define LEFT_SPC LT(1, KC_SPC)
-#define RGHT_SPC LT(1, KC_SPC)
+#define WLEFT_SPC LT(1, KC_SPC)
+#define WRGHT_SPC LT(1, KC_SPC)
+#define MLEFT_SPC LT(3, KC_SPC)
+#define MRGHT_SPC LT(3, KC_SPC)
+
+
 #define Q_TAB LALT_T(KC_Q)
 #define F_GUI LGUI_T(KC_F)
 #define D_ALT LALT_T(KC_D)
@@ -29,8 +33,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define J_CTL LCTL_T(KC_J)
 #define MINSGUI  LGUI_T(KC_MINS)
 // #define MINSMED  LT(3, KC_MINS)
-#define QUOTMED  LT(3, KC_QUOT)
-#define MV_ENT   LT(2, KC_ENTER)
+#define QUOTMED  LT(5, KC_QUOT)
+#define MV_ENT   LT(4, KC_ENTER)
 #define GRVMSE   LT(4, KC_GRAVE)
 #define T_QWERTY TO(0)
 #define T_GAMES  TO(5)
@@ -38,7 +42,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define HD_LSPC LT(1, KC_LALT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    // Normal base layer
+    // QWERTYWIN
     [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,   Q_TAB,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
@@ -47,11 +51,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  MV_ENT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            KC_NO, KC_NO,  LEFT_SPC,   RGHT_SPC, MINSGUI,   KC_NO
+                                            KC_NO,   MO(6),WLEFT_SPC, WRGHT_SPC, MINSGUI,   KC_NO
                                       //`--------------------------'  `--------------------------'
   ),
-    // Numbers and symbols (Activate: Either)
+    // Numbers and symbols (Activate: Either Space on QWERTYWIN)
     [1] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_ESC,  KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+         KC_0,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      _______, KC_NUBS, KC_BTN1, KC_BTN2,S(KC_LBRC),KC_LBRC,                    KC_RBRC,S(KC_RBRC),KC_MINS,KC_EQUAL, KC_BSLS,LGUI(KC_ENTER),
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          _______, _______, _______,    _______, _______, _______
+                                      //`--------------------------'  `--------------------------'
+  ),
+    // QWERTYMACOS
+    [2] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_TAB,   Q_TAB,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+       GRVMSE,    KC_A,   S_CTL,   D_ALT,   F_GUI,    KC_G,                         KC_H,   J_CTL,   K_ALT,   L_GUI, KC_SCLN, QUOTMED,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  MV_ENT,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                            KC_NO, KC_NO,  MLEFT_SPC,   MRGHT_SPC, MINSGUI,   KC_NO
+                                      //`--------------------------'  `--------------------------'
+  ),
+    // Numbers and symbols (Activate: Either Space on QWERTYMACOS)
+    [3] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_ESC,  KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -64,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
     // Movement (Activate: Enter)
-    [2] = LAYOUT_split_3x6_3(
+    [4] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_ESC,C(KC_LEFT), KC_UP,C(KC_RGHT), KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,C(KC_DEL), KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -77,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
        // Media (Activate: Minus Sign)
-    [3] = LAYOUT_split_3x6_3(
+    [5] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_VOLU,                        KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_MPRV,   KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
